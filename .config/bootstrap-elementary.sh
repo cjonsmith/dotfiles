e7a7ae6a97ec837742c08afe55fc800fb0bb8a59
Cameron Smith <c.jon.smith@gmail.com>
2019-03-22 23:46:37 -0700

    Remove unnecessary sudos

--- .config/bootstrap-elementary.sh
+++ .config/bootstrap-elementary.sh
@@ -32,8 +32,8 @@ installChrome() {
 }
 
 installMerge() {
-    curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+    curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+    echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
     apt update && apt -y install sublime-merge
 }
 
