23f970ded469ffce43be0be36aa7b318accce4df
Cameron Smith <cjonsmith@gmail.com>
2020-06-26 11:30:56 -0700

    Additional emacs configurations

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,3 +2,11 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
+;; Enable line numbers, globally
+(global-linum-mode 1)
+
+;; Enable Ido, globally
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
