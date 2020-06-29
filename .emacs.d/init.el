6fe00a776ecdf20f9eb27164b41da0df5202f9ff
Cameron Smith <cjonsmith@gmail.com>
2020-06-28 23:51:26 -0700

    Add MELPA support, include changes from installing magit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,12 @@
+;; Add MELPA (ELisp package archive)
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  )
+(package-initialize)
+
 ;; Disable menu-bar, tool-bar, and scroll-bar
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -17,3 +26,16 @@
 
 ;; Set the size of the frame
 (setq-default initial-frame-alist '((width . 135) (height . 55)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
