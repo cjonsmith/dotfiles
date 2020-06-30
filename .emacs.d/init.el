81ade31b833d2dc52049cbf08e0357b4f1c8faea
Cameron Smith <cjonsmith@gmail.com>
2020-06-29 22:35:53 -0700

    Enhancements for dired
    Enable dired-hide-details-mode by default and update listing switches

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,11 +31,17 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (setq mac-command-modifier 'meta)
 
+;; Enable dired-hide-details-mode minor mode in dired mode by default
+(add-hook 'dired-mode-hook
+	  (lambda ()
+	    (dired-hide-details-mode)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dired-listing-switches "-alGh --group-directories-first")
  '(package-selected-packages (quote (magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
