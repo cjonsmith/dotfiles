25ec4aff2359712286b67a9cfb10945e005676f9
Cameron Smith <cjonsmith@gmail.com>
2020-07-01 17:17:23 -0700

    Backup directories to .emacs.d directory

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -43,7 +43,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(dired-listing-switches "-alGh --group-directories-first")
+ '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
  '(package-selected-packages (quote (magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
