8be6a4614af2d0c5021ff222bd9bea455ba75326
Cameron Smith <cjonsmith@gmail.com>
2020-07-08 23:31:20 -0700

    Small config changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -43,7 +43,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
+ '(Info-isearch-search t)
+ '(org-startup-truncated nil)
  '(package-selected-packages (quote (magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
