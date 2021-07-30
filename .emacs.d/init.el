e0e1a35b1c5fd864cbfeb7328d40e1279bbc8b66
Cameron Smith <cjonsmith@gmail.com>
2021-07-29 21:14:48 -0700

    Docview and org image configurations.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,6 +16,9 @@
 (when (eq system-type 'gnu/linux)
   (tool-bar-mode 0))
 
+(setq doc-view-resolution 400)
+(setq org-image-actual-width nil)
+
 (defun get-ghes-releases ()
   "Fetches the current releases of GHES.
 
