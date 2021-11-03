6d6610d41f2e520cd88fef8d389728fe628975a6
Cameron Smith <cjonsmith@github.com>
2021-11-02 17:58:55 -0700

    Add typescript support and typescript LSP.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -194,6 +194,7 @@ If called with C-u, then only copy the name of the file."
   ((sh-mode . lsp)
    (ruby-mode . lsp)
    (go-mode . lsp)
+   (typescript-mode . lsp)
    (lsp-mode . lsp-enable-which-key-integration)))
 
 (use-package company
@@ -241,7 +242,7 @@ If called with C-u, then only copy the name of the file."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(request company-shell company project use-package yasnippet yaml-mode which-key wgrep smooth-scroll projectile-ripgrep origami nov mini-frame lsp-mode ido-vertical-mode go-mode forge flycheck fish-mode exec-path-from-shell dumb-jump dracula-theme chess buffer-move browse-at-remote atom-dark-theme async)))
+   '(typescript-mode request company-shell company project use-package yasnippet yaml-mode which-key wgrep smooth-scroll projectile-ripgrep origami nov mini-frame lsp-mode ido-vertical-mode go-mode forge flycheck fish-mode exec-path-from-shell dumb-jump dracula-theme chess buffer-move browse-at-remote atom-dark-theme async))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
