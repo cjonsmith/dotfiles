445ceb28d78c0a59227f5648ef444e918cad7b4d
Cameron Smith <cjonsmith@github.com>
2021-11-02 17:41:14 -0700

    Add custom function to copy filename.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -121,6 +121,16 @@ function."
 	    (osrs-get-shooting-stars))))
 
 ;; TODO Add a function to copy the path of the current buffer's file (and optionally line number) to kill-ring.
+(defun cjonsmith-copy-filename-as-kill ()
+  "Adds the filename (including path) of the current buffer to the kill ring.
+
+If called with C-u, then only copy the name of the file."
+  (interactive)
+  (cond
+   ((equal current-prefix-arg nil)
+    (kill-new (buffer-file-name)))
+   ((equal current-prefix-arg '(4))
+    (kill-new (file-name-nondirectory (buffer-file-name))))))
 
 ;; CAUTION: Be sure to reset this to the default value (10) if you're going to be making changes to a file remotely
 ;; outside of TRAMP or another user has access to the same files and will make changes as well.
