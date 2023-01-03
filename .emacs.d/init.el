0b6c641fac8f719ccbcc0aef868df8f1e6aae188
Cameron Smith <cjonsmith@github.com>
2023-01-03 15:56:45 -0800

    Install codespace package.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -265,6 +265,13 @@ If called with C-u, then only copy the name of the file."
   :config
   (setq graphviz-dot-indent-width 4))
 
+(use-package codespaces
+  :config
+  (if (executable-find "gh")
+      (codespaces-setup)
+    (message "Executable: `gh' was not found in `exec-path'.  Ensure that it is installed on your system if you wish to use GitHub Codespaces."))
+  :bind ("C-c S" . #'codespaces-connect))
+
 ;; In my experience, MacOS lacks any system default libraries that `hunspell' (the default spellchecker that comes with MacOS) can
 ;; access.  This may be a little heavy-handed to solve that problem, but by installing `aspell' it will also include several
 ;; different dictionaries along side the binary.  Switching to using `aspell' seems to be the quickest/least manual way of solving
@@ -287,6 +294,7 @@ If called with C-u, then only copy the name of the file."
  '(org-export-backends '(ascii html icalendar latex md odt))
  '(package-selected-packages
    '(terraform-mode graphviz-dot-mode orgit yafolding docker dockerfile-mode typescript-mode request company-shell company project use-package yasnippet yaml-mode which-key wgrep smooth-scroll projectile-ripgrep origami nov mini-frame lsp-mode ido-vertical-mode go-mode forge flycheck fish-mode exec-path-from-shell dumb-jump dracula-theme chess buffer-move browse-at-remote atom-dark-theme async))
+   '(codespaces terraform-mode graphviz-dot-mode orgit yafolding docker dockerfile-mode typescript-mode request company-shell company project use-package yasnippet yaml-mode which-key wgrep smooth-scroll projectile-ripgrep origami nov mini-frame lsp-mode ido-vertical-mode go-mode forge flycheck fish-mode exec-path-from-shell dumb-jump dracula-theme chess buffer-move browse-at-remote atom-dark-theme async))
  '(tramp-histfile-override nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
