bea786394835992c69e578d8d1a1f41a55563b1e
Cameron Smith <cjonsmith@github.com>
2023-01-15 22:04:55 -0800

    Change active mode-line font face.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -217,4 +217,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:background "plum1" :foreground "black" :box (:line-width (2 . 6) :style released-button))))))
