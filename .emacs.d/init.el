7f7e98ac8f4a3cddb2fc8e5e499c92682420412f
Cameron Smith <cjonsmith@github.com>
2023-01-15 22:19:03 -0800

    Info-quoted font face is now slanted.
    ...and implicitly inherits from the `default' font face. The default
    setting before was to inherit from `fixed-pitch-serif', which was
    noticibly smaller than the surrounding `default' face text in a Info
    document.  A slanted text should be enough visual difference.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -217,4 +217,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Info-quoted ((t (:slant italic))))
  '(mode-line ((t (:background "plum1" :foreground "black" :box (:line-width (2 . 6) :style released-button))))))
